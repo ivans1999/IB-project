60785ae26a78eb0164c612f6b543a1989346d8dd
Korisnik <Korisnik@IVAN>
2020-07-09 18:24:27 +0200

    Uneti sql podaci i dodati jks fajlovi u web app.

--- Projekat_export/IB_Project_Shell/src/main/resources/import.sql
+++ Projekat_export/IB_Project_Shell/src/main/resources/import.sql
@@ -1,1 +1,21 @@
+CREATE SCHEMA IB DEFAULT CHARACTER SET utf8;
 
+CREATE TABLE USER(
+	id INT AUTO_INCREMENT,
+    email VARCHAR(50) NOT NULL,
+	password VARCHAR(20) NOT NULL, 
+	certificate VARCHAR(40) NOT NULL, 
+	active INT(10),
+	authority VARCHAR(20) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+INSERT INTO USER (id, email, password, certificate, active, authority) VALUES (1, 'usera@gmail.com','usera', '' , 1, 'Admin');
+
+CREATE TABLE AUTHORITY(
+	id INT AUTO_INCREMENT,
+	name VARCHAR(10),
+    PRIMARY KEY(id)
+);
+INSERT INTO AUTHORITY (id, name) VALUES (1, 'Admin');
+INSERT INTO AUTHORITY (id, name) VALUES (2, 'Regular');
